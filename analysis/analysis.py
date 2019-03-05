2ec947e423ad4e3c39c20ebc7c9f5caba4b1b422
LYS <AlgerTK@163.com>
2019-03-05 18:33:17 +0800

    对银联的交易数据进行分析

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -4,14 +4,18 @@ import numpy as np
 import pandas as pd
 import json
 import os
-from data import load_tran_data
+#from data import load_tran_data
 
+def load_tran_data(csv_name, tran_path = 'D:\code\data'):
+    csv_path = os.path.join(tran_path, csv_name)
+    return pd.read_csv(csv_path, dtype = str)
 df = load_tran_data('unpy_devcod.csv')
 df_new = df[['custno', 'unpydt', 'tranti', 'acctno']]
 def gain_info(df):
     cust_no = list(set(df.custno))
     cust_len = len(cust_no)
-   
+    for it in range(cust_len):
+         
 
 
 
