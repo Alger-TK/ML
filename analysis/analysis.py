7d299f7ad19971886dc8e0ae5e541633f4eb1608
LYS <AlgerTK@163.com>
2019-03-05 14:13:06 +0800

    对银联的交易数据进行分析

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,2 +1,10 @@
-print('UESTC')
-a = 3
+"""agent_yl data analysising"""
+
+import numpy as np
+import pandas as pd
+
+def load_tran_data(csv_name, tran_path = 'D:\code\data'):
+    csv_path = os.path.join(tran_path, csv_name)
+    return pd.read_csv(csv_path)
+
+
