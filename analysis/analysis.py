f99994814097bbd039ba96c05130409c7a9921ee
LYS <AlgerTK@163.com>
2019-03-06 11:12:48 +0800

    对银联的交易数据进行分析

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -4,23 +4,22 @@ import numpy as np
 import pandas as pd
 import json
 import os
-#from data import load_tran_data
+from data import load_tran_data
 
-def load_tran_data(csv_name, tran_path = 'D:\code\data'):
-    csv_path = os.path.join(tran_path, csv_name)
-    return pd.read_csv(csv_path, dtype = str)
 df = load_tran_data('unpy_devcod.csv')
-df_new = df[['custno', 'unpydt', 'tranti', 'acctno']]
-# def gain_info(df):
-#     cust_no = list(set(df.custno))
-#     cust_len = len(cust_no)
 
-cust_no = list(set(df.custno))
-cust_len = len(cust_no)
-for it in range(cust_len):
-    df_per = df[df.custno == cust_no[it]]
-    if len(df_per) > 200:
-        break
+# obtain transaction data for a single customer
+def get_per_cust(df):
+    df_new = df[['custno', 'unpydt', 'tranti', 'acctno']]
+    cust_no = list(set(df.custno))
+    cust_len = len(cust_no)
+    for it in range(cust_len):
+        df_per = df[df.custno == cust_no[it]]
+        if len(df_per) > 200:
+            break
+    return df_per
+
+df_per = get_per_cust(df)
 df_per.to_csv('D:\code\data\df_per.csv')
 
 
--- analysis/perAnalysis.py
+++ analysis/perAnalysis.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import json
-import os
 from data import load_tran_data
 
 df_per = load_tran_data(csv_name = 'df_per.csv')
-print(df_per)
+print(df_per.info())
