65f2d5333a85905c389c2dda5d112cf2ea352150
LYS <AlgerTK@163.com>
2019-03-06 15:40:25 +0800

    对银联的交易数据进行分析

--- analysis/single.py
+++ analysis/single.py
@@ -1,7 +1,15 @@
 import numpy as np
 import pandas as pd
 import json
+from dateutil.parser import parse
 from data import load_tran_data
 
 df_per = load_tran_data(csv_name = 'df_per.csv')
-print(df_per.info())
+print(df_per.tran)
+
+
+def process(df_per):
+    # df_per_len: length of transaction information of single customer
+    df_per_len = len(df_per)
+
+
