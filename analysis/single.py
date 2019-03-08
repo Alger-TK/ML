7549b7810e976aa48a004d1d6712179a0338215f
LYS <AlgerTK@163.com>
2019-03-08 16:18:36 +0800

    对银联的交易数据进行分析

--- analysis/single.py
+++ analysis/single.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import json
+from pandas import DataFrame, Series
 from dateutil.parser import parse
 from data import load_tran_data
 
@@ -12,14 +13,30 @@ df_dic = {col: df_per[col].tolist() for col in df_per.columns}
 
 def process(df_dic, df_per_len):
     ls = [0 for i in range(df_per_len)]
+    dev = df_dic['devcod']
     """
     df_per_len:lenggth of transaction information of single customer
     df_dic: change dataframe to dict type 
     
     """
+
     return ls
 
-print(df_dic)
+def count_card(df_dic, df_per_len):
+    card = df_dic['acctno']
+    count_card = len(list(set(card)))
+    return count_card
+
+
+
+
+c = count_card(df_dic, df_per_len)
+card_num = [c for i in range(df_per_len)]
+df_dic['card_num'] = card_num
+df_per = DataFrame(df_dic)
+print(df_per.info())
+print(df_per)
+
 
 
 
