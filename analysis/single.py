ed6071a680b7b2678963735a3296434004c9684c
LYS <AlgerTK@163.com>
2019-03-12 14:29:42 +0800

    对银联的交易数据进行分析

--- analysis/single.py
+++ analysis/single.py
@@ -35,25 +35,55 @@ def detail(df_per = df_per):
     trantime = df_dic['trantime']
     trancode = df_dic['trancd']
     devcode = df_dic['devcod']
+    card = df_dic['acctno']
+    lat = df_dic['lat']
+    lng = df_dic['lng']
+
     for in1 in range(df_per_len):
-        count_pos_1 = 0
-        count_pos_2 = 0
+        count = [0 for i in range(28)]
         for in2 in range(df_per_len):
             t1 = parse(trantime[in1])
             t2 = parse(trantime[in2])
             if t1 >= t2 and (t1 - t2).total_seconds() <= 600 \
-            and trancode[in2] == '02000000' :
+            and trancode[in2] == '02000000' and \
+            card[in1] == card[in2]:
                 if devcode[in1] == devcode[in2]:
-                    count_pos_1 = count_pos_1 + 1
-                    ls[0][in1] = count_pos_1
+                    count[0] = count[0] + 1
+                    ls[0][in1] = count[0]
                 else:
-                    count_pos_2 = count_pos_2+1
-                    ls[1][in1] = count_pos_2
-    df_per.insert(8,'tran_5_pos', ls[0])
-    df_per.insert(9,'tran_5_pos_area', ls[1])
+                    count[1] = count[1] + 1
+                    ls[1][in1] = count[1]
+    df_per.insert(8, 'tran_5_pos', ls[0])
+    df_per.insert(9, 'tran_5_pos_area', ls[1])
     return df_per
 
-detail(df_per)
+def detail_1(df_per = df_per):
+    df_per_len = len(df_per)
+    df_dic = {col:df_per[col].tolist() for col in df_per.columns}
+    ls = []
+    for i in range(28):
+        ls.append([0 for i in range(df_per_len)])
+    trantime = df_dic['trantime']
+    trancode = df_dic['trancd']
+    devcode = df_dic['devcod']
+    card = df_dic['acctno']
+    lat = df_dic['lat']
+    lng = df_dic['lng']
+
+    for in1 in range(df_per_len):
+        count = [0 for i in range(28)]
+        for in2 in range(df_per_len):
+            t1 = parse(trantime[in1])
+            t2 = parse(trantime[in2])
+            if t1 >= t2 and (t1 - t2).total_seconds() <= 24*3600 and \
+            trancode[in2] == '02000102' and card[in1] == card[in2]:
+                count[2] = count[2] + 1
+                ls[2][in1] = count[2]
+    df_per.insert(10, 'tran_dev_area', ls[2])
+    print(ls[2])
+    return df_per
+
+detail_1(df_per)
 df_per.to_csv('D:/code/data/df_per_new_2.csv')
 
 
