6babbbfc93e239b42593e9b827a786ab204b0a71
LYS <AlgerTK@163.com>
2019-03-14 16:50:19 +0800

    对银联的交易数据进行分析

--- analysis/all.py
+++ analysis/all.py
@@ -9,14 +9,14 @@ custno = list(set(df.custno))
 
 df_new = load_tran_data(csv_name = 'df_new_model.csv')
 
-
-
-for cust in custno:
-    df_per = df[df.custno == cust]
-    df_per = detail_1(df_per)
-    df_new = df_new.append(df_per)
-
-df_new.to_csv('D:/code/data/df_ana_new.csv', index = False)
+print(len(custno))
+# custno1 = custno[:1000]
+# for cust in custno1:
+#     df_per = df[df.custno == cust]
+#     df_per = detail_1(df_per)
+#     df_new = df_new.append(df_per)
+#
+# df_new.to_csv('D:/code/data/df_ana_new.csv', index = False)
 
 
 
